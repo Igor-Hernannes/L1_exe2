c8355b0a142a62743c7087de515ce7e01f67b283
Igor Hernannes <rm98782@estudante.fieb.edu.br>
2026-06-01 10:35:59 -0300

    router.js atualizado
    Foi adicionado o express-validator

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,40 +1,60 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
-
-router.get("/", (req, res)=>{
-    res.render("pages/index",{"retorno":null,"valores":{"dia":""}});
+router.get("/", (req, res) => {
+   res.render("pages/index", {
+       erros: null,
+       retorno: null,
+       valores: {
+           dia: ""
+       }
+   });
 });
-
-
-router.post("/classificar", (req, res)=>{
-
-    //recuperar a idade do nadador
-    let dia = parseInt(req.body.dia);
-
-    //manipular os dados -> classificar
-    if(dia == 1){
-        var categoria = "Domingo";
-    }else if(dia == 2){
-        var categoria = "Segunda";
-    }else if(dia == 3){
-        var categoria = "Terça";
-    }else if(dia == 4){
-        var categoria = "Quarta";
-    }else if(dia == 5 ){
-        var categoria = "Quinta";
-    }else if(dia == 6 ){
-        var categoria = "Sexta";
-    }else if(dia == 7){
-        var categoria = "Sábado";
-    }else { var categoria = "Dia invalido!!"; }
-
-    //formatação 
-    let objJson = {"categoria":categoria};
-
-    //envio dos dados para mescalr com o HTML
-    res.render("pages/index",{"retorno":objJson,"valores":{"dia":req.body.dia}})
-
-});
-
-
+router.post(
+   "/classificar",
+   [
+       body("dia")
+           .notEmpty().withMessage("O dia é obrigatório.")
+           .isInt({ min: 1, max: 7 }).withMessage("Digite um número entre 1 e 7.")
+   ],
+   (req, res) => {
+       const erros = validationResult(req);
+       if (!erros.isEmpty()) {
+           return res.render("pages/index", {
+               erros: erros.array(),
+               retorno: null,
+               valores: {
+                   dia: req.body.dia
+               }
+           });
+       }
+       let dia = parseInt(req.body.dia);
+       let categoria;
+       if (dia == 1) {
+           categoria = "Domingo";
+       } else if (dia == 2) {
+           categoria = "Segunda";
+       } else if (dia == 3) {
+           categoria = "Terça";
+       } else if (dia == 4) {
+           categoria = "Quarta";
+       } else if (dia == 5) {
+           categoria = "Quinta";
+       } else if (dia == 6) {
+           categoria = "Sexta";
+       } else if (dia == 7) {
+           categoria = "Sábado";
+       }
+       let objJson = {
+           categoria: categoria
+       };
+       res.render("pages/index", {
+           erros: null,
+           retorno: objJson,
+           valores: {
+               dia: req.body.dia
+           }
+       });
+   }
+);
 module.exports = router;
